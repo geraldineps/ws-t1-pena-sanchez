672fdb3589576f636569e22b6bc62feda774e56a
graldineps <geriifujioka@gmail.com>
2024-09-23 22:50:45 -0500

    Examen T1

---
+++ src/main/java/pe/edu/cibertec/ws_t1_pena_sanchez/WsT1PenaSanchezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ws_t1_pena_sanchez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsT1PenaSanchezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsT1PenaSanchezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/ws_t1_pena_sanchez/WsT1PenaSanchezApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ws_t1_pena_sanchez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsT1PenaSanchezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
